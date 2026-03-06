4e5217a3322e1835d7ee4aaba4fdd52ec2509873
patunood6-bit <p.atunood6@gmail.com>
2026-03-06 22:49:36 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-        base: '/Elite12/,
+        base: '/elite12/,
       server: {
         port: 3000,
         host: '0.0.0.0',
